3089bf74f31dc0ac4c8c758a005ce712752a73b3
Cameron Barr <cjbarr1013@gmail.com>
2026-02-05 16:09:02 -0500

    add placeholder text

--- public/js/uploadNameCheckbox.js
+++ public/js/uploadNameCheckbox.js
@@ -3,4 +3,8 @@ const nameInput = document.getElementById('uploadName');
 
 checkbox.addEventListener('change', () => {
   nameInput.disabled = checkbox.checked;
+  nameInput.placeholder = checkbox.checked ? 'default' : '';
+  nameInput.value = '';
 });
+
+nameInput.placeholder = checkbox.checked ? 'default' : '';
